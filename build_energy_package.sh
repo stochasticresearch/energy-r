365647d1ff07a528f08aeb15958c62b30187e9e1
Kiran Karra <kiran.karra@gmail.com>
2016-10-14 08:19:40 -0400

    upgraded energy package from 1.6.2 to 1.7.0

--- build_energy_package.sh
+++ build_energy_package.sh
@@ -4,13 +4,13 @@ reset
 pushd .
 
 # Remove Old versions
-rm energy_1.6.2.tar.gz
+rm energy_1.7-0.tar.gz
 R CMD REMOVE --library=../install energy
 
 # build new version
 R CMD build --no-build-vignettes --no-manual energy
 
 # install in our directory
-R CMD INSTALL --library=../install energy_1.6.2.tar.gz 
+R CMD INSTALL --library=../install energy_1.7-0.tar.gz 
 
 popd
